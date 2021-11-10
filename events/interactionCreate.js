a5a56e4c8407afd44eed6eba13965a7aaa31e672
LoukHin <loukhin3141@gmail.com>
2021-11-11 01:38:41 +0700

    feat: improve error handling

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -12,10 +12,15 @@ module.exports = {
         await command.execute(interaction)
       } catch (error) {
         console.error(error)
-        await interaction.reply({
+        const reply = {
           content: 'There was an error while executing this command!',
           ephemeral: true
-        })
+        }
+        if (interaction.deferred) {
+          await interaction.editReply(reply)
+        } else {
+          await interaction.reply(reply)
+        }
       }
     }
   }
