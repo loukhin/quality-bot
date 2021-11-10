ee3e6e1f3d5d7ff751ffbe0dc4ddcd3295c7cd65
LoukHin <loukhin3141@gmail.com>
2021-11-11 01:39:09 +0700

    feat: improve message deletion logic

--- events/messageReactionAdd.js
+++ events/messageReactionAdd.js
@@ -9,21 +9,30 @@ module.exports = {
         return
       }
     }
+    const reactedUser = await reaction.users.fetch()
     const isNotSelfMessage = reaction.message.author.id !== reaction.client.user.id
     const isSelfReaction = user.id === reaction.client.user.id
+    const isNotDeletable = !reactedUser.has(reaction.client.user.id)
     const isReplying = reaction.message.mentions.repliedUser !== null
-    const isNotRepliedUserReaction = user.id === reaction.message.mentions.repliedUser
-    if (isNotSelfMessage || isSelfReaction || isNotRepliedUserReaction || !reaction.me) return
+    const isNotMentionedUserReaction = !reaction.message.mentions.users.has(user.id)
+    const isNotDeleteEmoji = reaction.emoji.name !== '❌'
 
-    const isDeleteEmoji = reaction.emoji.name === '❌'
+    if (
+      isNotDeleteEmoji ||
+      isNotSelfMessage ||
+      isSelfReaction ||
+      isNotDeletable ||
+      isNotMentionedUserReaction
+    )
+      return
 
-    if (isDeleteEmoji) {
-      if (isReplying) {
+    if (isReplying) {
+      try {
         const channel = await reaction.client.channels.fetch(reaction.message.channelId)
         const userFile = await channel.messages.fetch(reaction.message.reference.messageId)
         userFile.delete()
-      }
-      reaction.message.delete()
+      } catch {}
     }
+    reaction.message.delete()
   }
 }
