a60c708c6c0c43295ea18e5c42c10f674e929aa9
LoukHin <loukhin3141@gmail.com>
2021-12-09 12:17:57 +0700

    feat: bot can now use file converter

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -6,28 +6,41 @@ module.exports = {
     if (interaction.isSelectMenu()) {
       if (interaction.customId === 'file-type') {
         interaction.deferReply()
-        await interaction.channel.messages.edit(interaction.message, {
-          content: 'Converting your file...',
-          components: []
-        })
-        const originalMessageId = interaction.message.reference.messageId
-        const originalMessage = await interaction.channel.messages.fetch(originalMessageId)
-        const attachment = originalMessage.attachments.values().next().value
-        const payload = {
-          source: {
-            kind: attachment.contentType?.split('/')[0],
-            url: attachment.url,
-            name: attachment.name
-          },
-          target: {
-            type: interaction.values[0]
+        try {
+          await interaction.channel.messages.edit(interaction.message, {
+            content: 'Converting your file...',
+            components: []
+          })
+          const originalMessageId = interaction.message.reference.messageId
+          const originalMessage = await interaction.channel.messages.fetch(originalMessageId)
+          const attachment = originalMessage.attachments.values().next().value
+          const payload = {
+            source: {
+              kind: attachment.contentType?.split('/')[0],
+              url: attachment.url,
+              name: attachment.name
+            },
+            target: {
+              type: interaction.values[0]
+            }
+          }
+          const { Payload } = await convertFile(payload)
+          const { result } = JSON.parse(Payload)
+          await interaction.channel.messages.edit(interaction.message, {
+            content: `You can download your file at: ${result.Location}`
+          })
+          interaction.deleteReply()
+        } catch {
+          const reply = {
+            content: 'There was an error while executing this command!',
+            ephemeral: true
+          }
+          if (interaction.deferred) {
+            await interaction.editReply(reply)
+          } else {
+            await interaction.reply(reply)
           }
         }
-        const { result } = await convertFile(payload)
-        await interaction.channel.messages.edit(interaction.message, {
-          content: `You can download your file at: ${result.Location}`
-        })
-        interaction.deleteReply()
       }
     } else if (interaction.isCommand()) {
       const command = interaction.client.commands.get(interaction.commandName)
